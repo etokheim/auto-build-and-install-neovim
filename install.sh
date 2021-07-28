d84ee70db4fc6d0fb8e33fac9fcb1a2e734c40fb
Erling Tokheim <e@tokheim.no>
2021-07-28 09:33:42 +0100

    Feat: Automatically configure nvim

--- install.sh
+++ install.sh
@@ -13,3 +13,14 @@ make -j4
 
 echo "Installing to /usr/local"
 sudo make install
+
+echo "Configuring Neovim"
+git clone https://github.com/etokheim/nvim-config.git
+mkdir -p ~/.config/nvim
+cp nvim-config/init.vim ~/.config/nvim/
+
+# Install vim-plug (plugin manager for vim/neovim)
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+echo "Run :PlugInstall when opening Neovim to install the plugins"
