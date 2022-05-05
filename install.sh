2eeae9daefd359defbfd258c43eea2002c038721
Erling Tokheim <e@tokheim.no>
2022-05-05 13:37:20 +0200

    Feat: Improve script
    The script should now also work on normal computers again.

--- install.sh
+++ install.sh
@@ -5,17 +5,27 @@ echo "---"
 sudo apt-get update -y
 sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl git
 
+echo ""
+echo ""
+echo "Cloning neovim's git repository"
+echo "---"
 git clone https://github.com/neovim/neovim
 
 cd neovim
 
 echo ""
 echo ""
-echo "Select the stable version"
+echo "Selecting the stable branch"
 echo "---"
 git checkout stable
 
-make -j4
+echo ""
+echo ""
+echo "Compiling the release version of neovim"
+echo "---"
+# Set the type of build (Release/Debug/RelWIthDebInfo)
+# -j flag shouldn't be added if ninja is installed
+make CMAKE_BUILD_TYPE=Release
 
 echo ""
 echo ""
@@ -37,5 +47,13 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 echo ""
 echo ""
-echo "Run :PlugInstall when opening Neovim to install the plugins"
+echo "Done!"
+echo "Successfully installed if build type is printed below:"
+./build/bin/nvim --version | grep ^Build
+echo ""
+echo ""
+echo "Things to do:"
+echo "1. Re-source your PATH. Ie.: `source ~/.bashrc`"
+echo "2. Open neovim and run `:PlugInstall` to install the plugins"
+echo "3. Delete the ./neovim folder if you don't need it"
 echo "---"
